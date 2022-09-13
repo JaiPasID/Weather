f5c169633dd8c825183243ec14441d605a73d177
IOS Gwenael <jaord.developpement@gmail.om>
2022-09-13 16:11:22 +0100

    Mise en place de Retrofit et Api

--- app/src/main/java/fr/jaipasid/weather/MainActivity.java
+++ app/src/main/java/fr/jaipasid/weather/MainActivity.java
@@ -3,12 +3,41 @@ package fr.jaipasid.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText mCountry;
+    String countryName;
+    TextView mAnswer;
+    Button mValidate;
+
+    private FetchDataViewModel fetchDataViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCountry = findViewById(R.id.countryName);
+        mAnswer = findViewById(R.id.answer_retrofit);
+        mValidate = findViewById(R.id.validate);
+
+
+
+        mValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countryName = mCountry.getText().toString();
+                fetchDataViewModel.getWeather(countryName);
+            }
+        });
+
     }
+
 }
